306f38c0a90cd8dd373ecb34812227ca86dcfef1
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-11-03 10:29:00 +0900

    first init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lld  long double
+#define MOD  10007
+#define INF 99999999
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+
+}
