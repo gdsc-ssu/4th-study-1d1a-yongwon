d31391ffacc118c27a6878f9092ca04dd42f1374
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-11-12 11:13:15 +0900

    1112 solved

---
+++ BOJ_15655.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lld  long double
+#define MOD  10007
+#define INF 99999999
+using namespace std;
+
+int A[10];
+int ans[10];
+bool vis[10];
+int n, m;
+void Back(int idx,int num) {
+    if(num==m) {
+        for(int i=0;i<m;i++) {
+            cout << ans[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=idx;i<n;i++) {
+        ans[num]=A[i];
+        if(vis[i])continue;
+        vis[i]=true;
+        Back(i+1,num+1);
+        vis[i]=false;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+        cin >> A[i];
+    sort(A,A+n);
+
+    Back(0,0);
+
+}
---
+++ BOJ_15656.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lld  long double
+#define MOD  10007
+#define INF 99999999
+using namespace std;
+
+int A[10];
+int ans[10];
+bool vis[10];
+int n, m;
+void Back(int num) {
+    if(num==m) {
+        for(int i=0;i<m;i++) {
+            cout << ans[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+
+    for(int i=0;i<n;i++) {
+        ans[num]=A[i];
+
+        Back(num+1);
+
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL);
+    cin >> n >> m;
+    for(int i=0;i<n;i++)
+        cin >> A[i];
+    sort(A,A+n);
+
+    Back(0);
+
+}
