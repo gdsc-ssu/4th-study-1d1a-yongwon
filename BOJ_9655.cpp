58c799d55dca80d61d98d2654883a220bdd8b83a
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-11-22 22:31:09 +0900

    1122 solved

---
+++ BOJ_9655.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define lld  long double
+#define MOD  10007
+#define INF 99999999
+using namespace std;
+
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    if (N % 2 == 1) {
+        cout << "SK";
+    }
+
+    else {
+        cout << "CY";
+    }
+
+}
