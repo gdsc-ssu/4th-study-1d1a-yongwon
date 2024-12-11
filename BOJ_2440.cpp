3bbd2ea4af6846346ac08f106983fc3383bfc9d3
Choi Yong Won <118504207+cywin1018@users.noreply.github.com>
2024-12-11 23:47:39 +0900

    1211 solved

--- BOJ_2440.cpp
+++ BOJ_2440.cpp
@@ -6,20 +6,17 @@
 using namespace std;
 
 
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-
     int N;
     cin >> N;
 
-    if (N % 2 == 1) {
-        cout << "SK";
+    for (int i = N; i > 0; i--) {
+        for (int j = 1; j <= i; j++)
+            cout << '*';
+        cout << '\n';
     }
 
-    else {
-        cout << "CY";
-    }
 
 }
